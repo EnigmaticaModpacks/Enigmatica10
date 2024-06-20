4bb91d1d4840333eb121bfbfeb7b577ef158f8eb
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2024-06-20 21:55:19 +0200

    Update prepare-for-release.ps1

--- automation/prepare-for-release.ps1
+++ automation/prepare-for-release.ps1
@@ -1,6 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$secretsFile = "secrets.ps1"
 function Validate-SecretsFile {
     if (!(Test-Path "$PSScriptRoot\$secretsFile")) {
         Write-Host "You need a valid CurseForge API Token in a $secretsFile file" -ForegroundColor Red
@@ -10,36 +9,79 @@ function Validate-SecretsFile {
 }
 
 . "$PSScriptRoot\settings.ps1"
-. "$PSScriptRoot\$secretsFile"
+. "$PSScriptRoot\secrets.ps1"
 
+function Get-ModloaderVersion {
+    # Define the path to your YAML file
+    $settingsPath = "$PSScriptRoot\settings.cfg"
+
+    # Load the YAML content from the file
+    $fileContent = Get-Content -Path $settingsPath -Raw
+
+    # Use regex to extract the loaderVersion
+    if ($fileContent -match "FORGEVER=\s*([^\s]+)") {
+        $loaderVersion = $matches[1] -replace ";"
+        Write-Output "loaderVersion: $loaderVersion"
+        return $loaderVersion
+    }
+    else {
+        throw "loaderVersion not found."
+    }
+}
 function Close-FixedIssues {
-    $Base64Token = [System.Convert]::ToBase64String([char[]]$GITHUB_TOKEN);
+    $base64Token = [System.Convert]::ToBase64String([char[]]$GITHUB_TOKEN);
     
-    $Headers = @{
-        Authorization = 'Basic {0}' -f $Base64Token
+    $headers = @{
+        Authorization = 'Basic {0}' -f $base64Token
         Accept        = 'application/vnd.github.v3+json'
     }
 
-    $Body = @{
-        labels = 'Status: Fixed In Next Release'
+    $body = @{
+        labels = 'fixed in next release'
         state  = 'open'
     }
     
-    $Uri = "https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPOSITORY/issues"
+    $uri = "https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPOSITORY/issues"
     $issues = Invoke-RestMethod -Headers $Headers -Uri $Uri -Body $Body
   
-    $Body = @{
+    $body = @{
         state = "closed"
     } | ConvertTo-Json
 
     $issues | ForEach-Object {
-        $IssueNumber = $_.number
+        $issueNumber = $_.number
         Write-Host "Closing issue: " -NoNewline
-        Write-Host "https://github.com/$GITHUB_NAME/$GITHUB_REPOSITORY/issues/$IssueNumber" -ForegroundColor Blue 
-        $Uri = "https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPOSITORY/issues/$IssueNumber"
+        Write-Host "https://github.com/$GITHUB_NAME/$GITHUB_REPOSITORY/issues/$issueNumber" -ForegroundColor Blue 
+        $Uri = "https://api.github.com/repos/$GITHUB_NAME/$GITHUB_REPOSITORY/issues/$issueNumber"
 
-        Invoke-RestMethod -Headers $Headers -Uri $Uri -Body $Body -Method Patch | Out-Null
+        Invoke-RestMethod -Headers $headers -Uri $uri -Body $body -Method Patch | Out-Null
     }
+
+    $modloaderVersion = Get-ModloaderVersion
+
+    Write-Host "-----"
+    Write-Host
+    Write-Host "### $CLIENT_FILE_DISPLAY_NAME"
+    Write-Host 
+    Write-Host "NeoForge-1.21.0-$modloaderVersion | [Mod Updates](https://github.com/EnigmaticaModpacks/$MODPACK_NAME/blob/master/changelogs/changelog_mods_$MODPACK_VERSION.md) | [Modlist](https://github.com/EnigmaticaModpacks/$MODPACK_NAME/blob/master/changelogs/modlist_$MODPACK_VERSION.md)"
+    Write-Host 
+    Write-Host "#### Added Mods"
+    Write-Host
+    Write-Host "#### Removed Mods"
+    Write-Host
+    Write-Host "#### Improvements"
+    Write-Host
+    Write-Host "#### Bugs Fixed"
+    Write-Host
+    $issues | ForEach-Object {
+        $isBug = $_.labels | Where-Object { $_.name -eq "bug" }
+
+        if ($isBug) {
+            Write-Host "* $($_.title) [(\#$($_.number))]($($_.html_url))" -ForegroundColor Green
+        }
+    }
+    Write-Host
+    Write-Host "---"
 
 }
 function Merge-DevelopIntoMasterPrompt {
