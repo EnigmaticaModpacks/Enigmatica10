1d369a20d1e59476e0d0543ba727a61523046e51
MuteTiefling <mutetiefling@gmail.com>
2024-07-11 12:57:52 -0400

    rename

--- kubejs/server_scripts/recipe_viewer/disable_items.js
+++ kubejs/server_scripts/recipe_viewer/disable_items.js
@@ -1,6 +1,5 @@
 RecipeViewerEvents.removeEntriesCompletely('item', (event) => {
     emi.items.disabled.forEach((stack) => {
-        console.log(`removing: ${stack}`);
         event.remove(stack);
     });
 });
