43f64e041f89d139c8a482550939482d5e716cb3
MuteTiefling <mutetiefling@gmail.com>
2024-07-13 08:21:09 -0400

    Update lockbox.js

--- kubejs/server_scripts/loot_tables/chests/aquaculture/lockbox.js
+++ kubejs/server_scripts/loot_tables/chests/aquaculture/lockbox.js
@@ -4,5 +4,13 @@ LootJS.lootTables((event) => {
         .getLootTable('aquaculture:box/lockbox')
         .firstPool()
         .addEntry(LootEntry.of('minecraft:ink_sac').withWeight(10))
+        .addEntry(LootEntry.of('ae2:certus_quartz_crystal').withWeight(8))
         .addEntry(LootEntry.of('minecraft:glow_ink_sac').withWeight(7));
+});
+
+LootJS.modifiers((event) => {
+    event
+        .addTableModifier('aquaculture:box/lockbox')
+        .removeLoot('minecraft:iron_ingot')
+        .removeLoot('minecraft:gold_ingot');
 });
