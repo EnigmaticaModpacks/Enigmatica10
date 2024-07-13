6b2f71591fdaae146b0441cff7662545195eed3d
MuteTiefling <mutetiefling@gmail.com>
2024-07-12 21:01:10 -0400

    Update neptunes_bounty.js

--- kubejs/server_scripts/loot_tables/chests/aquaculture/neptunes_bounty.js
+++ kubejs/server_scripts/loot_tables/chests/aquaculture/neptunes_bounty.js
@@ -7,7 +7,7 @@ LootJS.modifiers((event) => {
                 .bonusRolls(0)
                 .addEntry(LootEntry.of('chococraft:blue_chocobo_spawn_egg').withWeight(5))
                 .addEntry(LootEntry.of('minecraft:heart_of_the_sea').withWeight(10))
-                .addEntry(LootEntry.of('usefulhats:aquanaut_helmet').withWeight(5))
+                .addEntry(LootEntry.of('usefulhats:aquanaut_helmet').withWeight(2))
                 .addEntry(LootEntry.of('relics:jellyfish_necklace').withWeight(2));
         })
         .pool((pool) => {
