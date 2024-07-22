f0188d4ae7d2b83a65432a0ada862b0a753ca836
MuteTiefling <mutetiefling@gmail.com>
2024-07-21 21:06:24 -0400

    Update stonecutting.js

--- kubejs/server_scripts/recipes/minecraft/stonecutting.js
+++ kubejs/server_scripts/recipes/minecraft/stonecutting.js
@@ -4,20 +4,20 @@ ServerEvents.recipes((event) => {
     const recipes = [];
 
     // Tag conversion
-    let conversion_types = [];
-    conversion_types = conversion_types.concat(chipped_types).concat(chisel_types);
+    // let conversion_types = [];
+    // conversion_types = conversion_types.concat(chipped_types).concat(chisel_types);
 
-    conversion_types.forEach((tag) => {
-        let ingredient = Ingredient.of(tag);
-        ingredient.stacks.forEach((block) => {
-            let output = block.getId();
-            recipes.push({
-                output: output,
-                input: ingredient,
-                id: `${id_prefix}${output.replace(':', '_')}_from_${tag.substring(1).replace(':', '_')}`
-            });
-        });
-    });
+    // conversion_types.forEach((tag) => {
+    //     let ingredient = Ingredient.of(tag);
+    //     ingredient.stacks.forEach((block) => {
+    //         let output = block.getId();
+    //         recipes.push({
+    //             output: output,
+    //             input: ingredient,
+    //             id: `${id_prefix}${output.replace(':', '_')}_from_${tag.substring(1).replace(':', '_')}`
+    //         });
+    //     });
+    // });
 
     recipes.forEach((recipe) => {
         event.stonecutting(recipe.output, recipe.input).id(recipe.id);
