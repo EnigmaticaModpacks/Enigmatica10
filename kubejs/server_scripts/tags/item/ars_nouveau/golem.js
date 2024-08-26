456317920224b6567c06b7d75497601688555776
MuteTiefling <mutetiefling@gmail.com>
2024-08-26 08:57:33 -0400

    Update golem.js

--- kubejs/server_scripts/tags/item/ars_nouveau/golem.js
+++ kubejs/server_scripts/tags/item/ars_nouveau/golem.js
@@ -1,8 +1,4 @@
 ServerEvents.tags('item', (event) => {
     // Items golems can pick up
-    event.add('ars_nouveau:golem/shard', [
-        'minecraft:amethyst_shard',
-        'ae2:certus_quartz_crystal',
-        'justdirethings:time_crystal'
-    ]);
+    event.add('ars_nouveau:golem/shard', ['#c:gems']);
 });
