2b30686a04f24dfcb3437f745c48f2a56c303dde
MuteTiefling <mutetiefling@gmail.com>
2024-08-28 18:13:52 -0400

    sneaky trade for boo!

---
+++ kubejs/server_scripts/trades/wandering_trader.js
@@ -0,0 +1,7 @@
+MoreJS.wandererTrades((event) => {
+    // https://docs.almostreliable.com/morejs/wanderer-trades.html
+    // level 1 - normal trades
+    // level 2 - rare trades
+    // addTrade(level: number, input: Item | Item[], output: Item)
+    event.addTrade(2, Item.of('minecraft:emerald', 32), 'artifacts:helium_flamingo');
+});
