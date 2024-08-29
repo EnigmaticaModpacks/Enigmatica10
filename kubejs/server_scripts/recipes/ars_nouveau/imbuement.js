5730cc0bdda64326b33d3fa0103db0e9a7fa07d3
MuteTiefling <mutetiefling@gmail.com>
2024-08-29 10:22:42 -0400

    Update imbuement.js

--- kubejs/server_scripts/recipes/ars_nouveau/imbuement.js
+++ kubejs/server_scripts/recipes/ars_nouveau/imbuement.js
@@ -8,6 +8,82 @@ ServerEvents.recipes((event) => {
             pedestalItems: [],
             source: 1500,
             id: `ars_nouveau:imbuement_amethyst_block`
+        },
+        {
+            input: { tag: 'c:gems/source' },
+            output: { count: 1, id: 'ars_nouveau:air_essence' },
+            pedestalItems: [{ tag: 'c:feathers' }, { item: 'ars_nouveau:wilden_wing' }, { tag: 'minecraft:arrows' }],
+            source: 2000 * 3,
+            id: `ars_nouveau:air_essence`
+        },
+        {
+            input: { tag: 'c:storage_blocks/source' },
+            output: { id: 'ars_nouveau:air_essence', count: 4 },
+            pedestalItems: [{ tag: 'c:feathers' }, { item: 'ars_nouveau:wilden_wing' }, { tag: 'minecraft:arrows' }],
+            source: 2000 * 3,
+            id: `${id_prefix}charged_certus_quartz_crystal`
+        },
+        {
+            input: { tag: 'c:storage_blocks/source' },
+            output: { id: 'ars_nouveau:earth_essence', count: 4 },
+            pedestalItems: [{ tag: 'c:ingots/iron' }, { tag: 'c:seeds' }, { tag: 'minecraft:dirt' }],
+            source: 2000 * 3,
+            id: `${id_prefix}earth_essence`
+        },
+        {
+            input: { tag: 'c:storage_blocks/source' },
+            output: { id: 'ars_nouveau:fire_essence', count: 4 },
+            pedestalItems: [
+                { item: 'minecraft:flint_and_steel' },
+                { item: 'minecraft:torch' },
+                { item: 'minecraft:gunpowder' }
+            ],
+            source: 2000 * 3,
+            id: `${id_prefix}fire_essence`
+        },
+        {
+            input: { tag: 'c:storage_blocks/source' },
+            output: { id: 'ars_nouveau:water_essence', count: 4 },
+            pedestalItems: [
+                { item: 'minecraft:water_bucket' },
+                { item: 'minecraft:snow_block' },
+                { item: 'minecraft:kelp' }
+            ],
+            source: 2000 * 3,
+            id: `${id_prefix}water_essence`
+        },
+        {
+            input: { tag: 'c:storage_blocks/source' },
+            output: { id: 'ars_nouveau:manipulation_essence', count: 4 },
+            pedestalItems: [
+                { item: 'minecraft:stone_button' },
+                { item: 'minecraft:redstone' },
+                { item: 'minecraft:clock' }
+            ],
+            source: 2000 * 3,
+            id: `${id_prefix}manipulation_essence`
+        },
+        {
+            input: { tag: 'c:storage_blocks/source' },
+            output: { id: 'ars_nouveau:abjuration_essence', count: 4 },
+            pedestalItems: [
+                { item: 'minecraft:fermented_spider_eye' },
+                { item: 'minecraft:sugar' },
+                { item: 'minecraft:milk_bucket' }
+            ],
+            source: 2000 * 3,
+            id: `${id_prefix}abjuration_essence`
+        },
+        {
+            input: { tag: 'c:storage_blocks/source' },
+            output: { id: 'ars_nouveau:conjuration_essence', count: 4 },
+            pedestalItems: [
+                { item: 'ars_nouveau:wilden_horn' },
+                { item: 'ars_nouveau:starbuncle_shards' },
+                { item: 'minecraft:book' }
+            ],
+            source: 2000 * 3,
+            id: `${id_prefix}conjuration_essence`
         }
     ];
 
