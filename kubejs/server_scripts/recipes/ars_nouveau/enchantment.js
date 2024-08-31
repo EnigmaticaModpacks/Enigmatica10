d2949ec1db75b25e6ffb11a9a886e7be5576c0ad
MuteTiefling <mutetiefling@gmail.com>
2024-08-30 21:12:08 -0400

    quests, enchants

--- kubejs/server_scripts/recipes/ars_nouveau/enchantment.js
+++ kubejs/server_scripts/recipes/ars_nouveau/enchantment.js
@@ -17,6 +17,54 @@ ServerEvents.recipes((event) => {
             ],
             sourceCost: 9000,
             id: `${id_prefix}delicate_touch`
+        },
+        {
+            enchantment: 'apothic_spawners:capturing',
+            level: 1,
+            pedestalItems: [
+                { tag: 'c:essences/manipulation' },
+                { tag: 'c:gems/emerald' },
+                { tag: 'c:storage_blocks/source' },
+                { tag: 'c:storage_blocks/source' },
+                { tag: 'c:storage_blocks/source' },
+                { tag: 'c:storage_blocks/source' },
+                { tag: 'c:storage_blocks/source' },
+                { tag: 'c:storage_blocks/source' }
+            ],
+            sourceCost: 6000,
+            id: `${id_prefix}capturing_1`
+        },
+        {
+            enchantment: 'apothic_spawners:capturing',
+            level: 2,
+            pedestalItems: [
+                { tag: 'c:essences/manipulation' },
+                { tag: 'c:essences/manipulation' },
+                { tag: 'c:gems/emerald' },
+                { tag: 'c:gems/emerald' },
+                { tag: 'c:gems/emerald' },
+                { tag: 'c:gems/emerald' },
+                { tag: 'c:storage_blocks/source' },
+                { tag: 'c:storage_blocks/source' }
+            ],
+            sourceCost: 8000,
+            id: `${id_prefix}capturing_2`
+        },
+        {
+            enchantment: 'apothic_spawners:capturing',
+            level: 3,
+            pedestalItems: [
+                { tag: 'c:essences/manipulation' },
+                { tag: 'c:essences/manipulation' },
+                { tag: 'c:essences/manipulation' },
+                { tag: 'c:storage_blocks/emerald' },
+                { tag: 'c:storage_blocks/lapis' },
+                { tag: 'c:storage_blocks/lapis' },
+                { tag: 'c:storage_blocks/source' },
+                { tag: 'c:storage_blocks/source' }
+            ],
+            sourceCost: 9000,
+            id: `${id_prefix}capturing_3`
         }
     ];
 
