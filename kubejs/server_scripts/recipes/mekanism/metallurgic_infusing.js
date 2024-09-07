74d8ecebe0d3288ed68830c8dcb91cdaec6ca72a
MuteTiefling <mutetiefling@gmail.com>
2024-09-07 16:35:42 -0400

    mek and old change logs

--- kubejs/server_scripts/recipes/mekanism/metallurgic_infusing.js
+++ kubejs/server_scripts/recipes/mekanism/metallurgic_infusing.js
@@ -6,24 +6,28 @@ ServerEvents.recipes((event) => {
             output: { id: 'minecraft:rotten_flesh', count: 1 },
             item_input: { tag: 'c:foods/raw_meat', count: 1 },
             chemical_input: { amount: 10, tag: 'mekanism:bio' },
+            per_tick_usage: false,
             id: `${id_prefix}rotten_flesh`
         },
         {
             output: { id: 'minecraft:golden_carrot', count: 1 },
             item_input: { item: 'minecraft:carrot', count: 1 },
             chemical_input: { amount: 10, tag: 'mekanism:gold' },
+            per_tick_usage: false,
             id: `${id_prefix}golden_carrot`
         },
         {
             output: { id: 'minecraft:glistering_melon_slice', count: 1 },
             item_input: { item: 'minecraft:melon_slice', count: 1 },
             chemical_input: { amount: 10, tag: 'mekanism:gold' },
+            per_tick_usage: false,
             id: `${id_prefix}glistering_melon_slice`
         },
         {
             output: { id: 'minecraft:golden_apple', count: 1 },
             item_input: { item: 'minecraft:apple', count: 1 },
             chemical_input: { amount: 80, tag: 'mekanism:gold' },
+            per_tick_usage: false,
             id: `${id_prefix}golden_apple`
         }
     ];
