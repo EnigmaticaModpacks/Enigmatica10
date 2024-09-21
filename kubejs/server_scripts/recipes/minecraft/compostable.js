53323a66080633da665c786b874a869479b78e54
MuteTiefling <mutetiefling@gmail.com>
2024-09-21 14:49:42 -0400

    compost

---
+++ kubejs/server_scripts/recipes/minecraft/compostable.js
@@ -0,0 +1,20 @@
+ServerEvents.compostableRecipes((event) => {
+    const recipes = [
+        {
+            input: 'minecraft:egg',
+            chance: 0.35
+        },
+        {
+            input: '#c:foods/raw_meat',
+            chance: 0.65
+        },
+        {
+            input: '#c:foods/safe_raw_fish',
+            chance: 0.65
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        event.add(recipe.input, recipe.chance);
+    });
+});
