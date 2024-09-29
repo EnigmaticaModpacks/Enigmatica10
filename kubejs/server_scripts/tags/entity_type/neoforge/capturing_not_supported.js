a3aac01c50b13c4cd9cfd0a8e2997c233adeaa79
MuteTiefling <mutetiefling@gmail.com>
2024-09-29 12:02:52 -0400

    capturing

--- kubejs/server_scripts/tags/entity_type/neoforge/capturing_not_supported.js
+++ kubejs/server_scripts/tags/entity_type/neoforge/capturing_not_supported.js
@@ -1,4 +1,4 @@
 ServerEvents.tags('entity_type', (event) => {
-    let additions = ['justdirethings:creature_catcher'];
+    let additions = ['justdirethings:creature_catcher', 'industrialforegoing:launcher_projectile_entity'];
     event.get('c:capturing_not_supported').add(additions);
 });
