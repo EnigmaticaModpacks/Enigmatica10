42e492c06c0ebaf89d4db7938a5f89eb51242401
MuteTiefling <mutetiefling@gmail.com>
2024-09-29 12:08:12 -0400

    use config for ritual duration changes

--- kubejs/server_scripts/recipes/occultism/ritual.js
+++ kubejs/server_scripts/recipes/occultism/ritual.js
@@ -1,5 +1,6 @@
 ServerEvents.recipes((event) => {
     const id_prefix = 'enigmatica:occultism/ritual/';
+    const default_duration = 30;
     const recipes = [];
 
     event.forEachRecipe({ type: 'occultism:ritual' }, (r) => {
@@ -9,15 +10,20 @@ ServerEvents.recipes((event) => {
         if (recipe.spirit_max_age) {
             if (recipe_id.includes('_trader')) {
                 // Remove decay from Traders
-                delete recipe.spirit_max_age;
+                delete recipe.entity_to_summon_settings.spirit_max_age;
             } else {
                 // Reduce delay for all other spirits (weather/daytime rituals)
-                recipe.spirit_max_age = 10;
+                recipe.entity_to_summon_settings.spirit_max_age = 10;
             }
         }
 
         // Override default craft time for all recipes
-        recipe.duration = 20;
+        // if (recipe.ritual_requirement_settings.duration) {
+        //     recipe.ritual_requirement_settings.duration = recipe.ritual_requirement_settings.duration / 10;
+        // } else {
+        //     recipe.ritual_requirement_settings.duration = default_duration / 10;
+        // }
+
         recipe.id = recipe_id;
         recipes.push(recipe);
     });
