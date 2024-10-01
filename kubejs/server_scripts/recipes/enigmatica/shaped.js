3e1994f17fa8b3c0ab441808b72c1b245ba4616d
MuteTiefling <mutetiefling@gmail.com>
2024-09-30 23:28:36 -0400

    IF quests and bonus gears

--- kubejs/server_scripts/recipes/enigmatica/shaped.js
+++ kubejs/server_scripts/recipes/enigmatica/shaped.js
@@ -3,12 +3,42 @@ ServerEvents.recipes((event) => {
 
     const recipes = [
         {
-            output: Item.of('minecraft:red_dye', 2),
+            output: '2x minecraft:red_dye',
             pattern: ['A ', ' A'],
             key: {
                 A: 'productivetrees:haematoxylin'
             },
             id: `${id_prefix}red_dye_from_haematoxylin`
+        },
+        {
+            output: '2x industrialforegoing:diamond_gear',
+            pattern: ['ABA', 'BCB', 'ABA'],
+            key: {
+                A: '#c:nuggets/dark_steel',
+                B: '#c:gems/diamond',
+                C: '#c:dusts/grains_of_infinity'
+            },
+            id: `${id_prefix}diamond_gear`
+        },
+        {
+            output: '2x modern_industrialization:gold_gear',
+            pattern: ['ABA', 'BCB', 'ABA'],
+            key: {
+                A: '#c:nuggets/gold',
+                B: '#c:ingots/gold',
+                C: '#c:dusts/grains_of_infinity'
+            },
+            id: `${id_prefix}gold_gear`
+        },
+        {
+            output: '2x pneumaticcraft:compressed_iron_gear',
+            pattern: ['ABA', 'BCB', 'ABA'],
+            key: {
+                A: '#c:nuggets/dark_steel',
+                B: '#c:ingots/compressed_iron',
+                C: '#c:dusts/grains_of_infinity'
+            },
+            id: `${id_prefix}compressed_iron_gear`
         }
     ];
 
