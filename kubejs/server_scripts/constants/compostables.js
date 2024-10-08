977f1066a9a5ab84be9d0802944f2c6c811ef785
MuteTiefling <mutetiefling@gmail.com>
2024-10-08 09:06:24 -0400

    compost

---
+++ kubejs/server_scripts/constants/compostables.js
@@ -0,0 +1,16 @@
+//priority: 1001
+
+const compostables = [
+    {
+        input: 'minecraft:egg',
+        chance: 0.35
+    },
+    {
+        input: '#c:foods/raw_meat',
+        chance: 0.65
+    },
+    {
+        input: '#c:foods/safe_raw_fish',
+        chance: 0.45
+    }
+];
--- kubejs/server_scripts/recipes/minecraft/compostable.js
+++ kubejs/server_scripts/recipes/minecraft/compostable.js
@@ -1,20 +1,15 @@
 ServerEvents.compostableRecipes((event) => {
-    const recipes = [
-        {
-            input: 'minecraft:egg',
-            chance: 0.35
-        },
-        {
-            input: '#c:foods/raw_meat',
-            chance: 0.65
-        },
-        {
-            input: '#c:foods/safe_raw_fish',
-            chance: 0.45
-        }
-    ];
-
-    recipes.forEach((recipe) => {
-        event.add(recipe.input, recipe.chance);
+    compostables.forEach((compostable) => {
+        event.add(compostable.input, compostable.chance);
     });
+});
+
+ServerEvents.generateData('before_mods', (event) => {
+    let data_map = { values: {} };
+    compostables.forEach((compostable) => {
+        // Build up compostables data map for NeoForge
+        data_map.values[compostable.input] = { chance: compostable.chance };
+    });
+
+    event.json(`neoforge:data_maps/item/compostables.json`, data_map);
 });
