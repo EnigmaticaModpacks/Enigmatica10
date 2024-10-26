0ebdaab6b19f9ccd8232eb5a3790d2c478e6e912
MuteTiefling <mutetiefling@gmail.com>
2024-10-25 23:45:56 -0400

    quest

--- kubejs/server_scripts/recipes/occultism/ritual.js
+++ kubejs/server_scripts/recipes/occultism/ritual.js
@@ -24,17 +24,13 @@ ServerEvents.recipes((event) => {
                 tag: 'c:llamas'
             };
             recipe.id = recipe_id;
-        }
-
-        if (recipe_id.includes('ritual/possess_warden')) {
+        } else if (recipe_id.includes('ritual/possess_warden')) {
             recipe.entity_to_sacrifice = {
                 display_name: 'ritual.occultism.sacrifice.bats',
                 tag: 'c:bats'
             };
             recipe.id = recipe_id;
-        }
-
-        if (
+        } else if (
             recipe_id.includes('ritual/possess_goat') ||
             recipe_id.includes('ritual/possess_witch') ||
             recipe_id.includes('ritual/possess_bee')
@@ -44,15 +40,44 @@ ServerEvents.recipes((event) => {
                 tag: 'c:chickens'
             };
             recipe.id = recipe_id;
-        }
-
-        if (recipe_id.includes('ritual/craft_budding_amethyst')) {
+        } else if (recipe_id.includes('ritual/craft_budding_amethyst')) {
             delete recipe.entity_to_sacrifice;
             recipe.ingredients = [
                 { tag: 'c:dusts/amethyst' },
                 { tag: 'c:dusts/amethyst' },
                 { tag: 'c:dusts/amethyst' },
                 { tag: 'c:dusts/amethyst' }
+            ];
+            recipe.id = recipe_id;
+            recipes.push(recipe);
+        } else if (recipe_id.includes('ritual/repair_miners')) {
+            recipe.ingredients = [
+                { item: 'occultism:otherworld_essence' },
+                { item: 'occultism:otherworld_essence' },
+                { item: 'occultism:otherworld_essence' },
+                { item: 'occultism:otherworld_essence' },
+                { item: 'occultism:spirit_attuned_gem' },
+                { item: 'occultism:spirit_attuned_gem' },
+                { item: 'occultism:spirit_attuned_gem' },
+                { item: 'occultism:spirit_attuned_gem' }
+            ];
+            recipe.id = recipe_id;
+            recipes.push(recipe);
+        } else if (recipe_id.includes('ritual/repair_armors') || recipe_id.includes('ritual/repair_tools')) {
+            recipe.ingredients = [
+                { item: 'occultism:otherworld_essence' },
+                { item: 'occultism:otherworld_essence' },
+                { item: 'occultism:spirit_attuned_gem' },
+                { item: 'occultism:otherworld_essence' }
+            ];
+            recipe.id = recipe_id;
+            recipes.push(recipe);
+        } else if (recipe_id.includes('ritual/repair_chalks')) {
+            recipe.ingredients = [
+                { item: 'occultism:otherworld_essence' },
+                { tag: 'c:dusts/otherworld_wood' },
+                { item: 'occultism:otherworld_essence' },
+                { tag: 'c:dusts/otherstone' }
             ];
             recipe.id = recipe_id;
             recipes.push(recipe);
