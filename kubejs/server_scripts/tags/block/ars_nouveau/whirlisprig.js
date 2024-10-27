32d86552ae4b2d2a2bcec29633a2c7e05763df0b
MuteTiefling <mutetiefling@gmail.com>
2024-10-27 12:52:14 -0400

    Create whirlisprig.js

---
+++ kubejs/server_scripts/tags/block/ars_nouveau/whirlisprig.js
@@ -0,0 +1,14 @@
+ServerEvents.tags('block', (event) => {
+    event.add('ars_nouveau:whirlisprig/greatly_likes', [
+        'deeperdarker:gloomy_cactus',
+        'deeperdarker:sculk_gleam',
+        'minecraft:melon',
+        'minecraft:pumpkin',
+        'naturesaura:aura_bloom',
+        'naturesaura:aura_cactus',
+        'naturesaura:warped_aura_mushroom',
+        'naturesaura:crimson_aura_mushroom',
+        'naturesaura:aura_mushroom'
+    ]);
+    event.add('ars_nouveau:whirlisprig/kinda_likes', []);
+});
