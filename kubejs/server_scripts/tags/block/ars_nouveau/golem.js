aeba645054531d42ad8f46f1cd9c99decb9f8590
MuteTiefling <mutetiefling@gmail.com>
2024-11-11 16:22:10 -0500

    Update golem.js

--- kubejs/server_scripts/tags/block/ars_nouveau/golem.js
+++ kubejs/server_scripts/tags/block/ars_nouveau/golem.js
@@ -2,5 +2,5 @@ ServerEvents.tags('block', (event) => {
     // Blocks to be accelerated by golems
     event.add('ars_nouveau:golem/budding', ['#c:budding_blocks', '#c:budding']);
     // Blocks to be broken by golems
-    event.add('ars_nouveau:golem/cluster', ['#c:buds']);
+    event.add('ars_nouveau:golem/cluster', ['#c:clusters']);
 });
