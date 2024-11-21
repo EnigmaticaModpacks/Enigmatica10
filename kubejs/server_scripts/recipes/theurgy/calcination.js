8c73effbdeb74e6a6cf13d46d19de626bd1c9f13
MuteTiefling <mutetiefling@gmail.com>
2024-11-21 08:26:16 -0500

    alternate salt recipes

--- kubejs/server_scripts/recipes/theurgy/calcination.js
+++ kubejs/server_scripts/recipes/theurgy/calcination.js
@@ -1,6 +1,29 @@
 ServerEvents.recipes((event) => {
     const id_prefix = 'enigmatica:theurgy/calcination/';
-    const recipes = [];
+    const recipes = [
+        {
+            ingredient: { ingredient: { tag: 'minecraft:meat' }, count: 1 },
+            result: {
+                type: 'theurgy:item',
+                id: 'theurgy:alchemical_salt_creature',
+                count: 1
+            },
+            time: 20,
+            category: 'misc',
+            id: `${id_prefix}alchemical_salt_creature_from_meat`
+        },
+        {
+            ingredient: { ingredient: { tag: 'minecraft:fishes' }, count: 1 },
+            result: {
+                type: 'theurgy:item',
+                id: 'theurgy:alchemical_salt_creature',
+                count: 1
+            },
+            time: 20,
+            category: 'misc',
+            id: `${id_prefix}alchemical_salt_creature_from_fishes`
+        }
+    ];
 
     event.forEachRecipe({ type: 'theurgy:calcination' }, (r) => {
         let recipe = JSON.parse(r.json);
