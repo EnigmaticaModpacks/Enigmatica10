5b229e88ba31150dcf94930db8c4148671ea557c
MuteTiefling <mutetiefling@gmail.com>
2024-11-24 09:28:37 -0500

    text fixes

--- kubejs/startup_scripts/item_registry.js
+++ kubejs/startup_scripts/item_registry.js
@@ -1,7 +1,11 @@
 //priority: 900
 StartupEvents.registry('item', (event) => {
     const ritualDummies = [
-        { id: 'occultism:ritual_dummy/misc_eldritch_manuscript', type: 'misc', tooltip: 'Obtain Eldritch Knowledge' }
+        {
+            id: 'occultism:ritual_dummy/misc_eldritch_manuscript',
+            type: 'misc',
+            tooltip: 'Obtain an Eldritch Manuscript at the small cost of your sanity.'
+        }
     ];
 
     ritualDummies.forEach((item) => {
