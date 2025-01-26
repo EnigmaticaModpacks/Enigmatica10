f981af418490cfe05d4fb467b5e4b066cc0fe9cf
MuteTiefling <mutetiefling@gmail.com>
2025-01-25 20:19:40 -0500

    chalk

--- kubejs/server_scripts/recipes/occultism/shapeless.js
+++ kubejs/server_scripts/recipes/occultism/shapeless.js
@@ -3,14 +3,29 @@ ServerEvents.recipes((event) => {
 
     const recipes = [
         {
+            output: 'occultism:chalk_purple_impure',
+            inputs: [
+                'occultism:chalk_light_gray_impure',
+                '#c:dusts/end_stone',
+                '#c:dusts/obsidian',
+                '#c:dusts/obsidian'
+            ],
+            id: `occultism:crafting/chalk_purple_impure`
+        },
+        {
             output: 'occultism:chalk_red_impure',
             inputs: [
-                'occultism:chalk_white_impure',
+                'occultism:chalk_gray_impure',
                 'occultism:afrit_essence',
                 'minecraft:nether_wart',
-                '#c:dusts/redstone'
+                'occultism:demonic_meat'
             ],
             id: `occultism:crafting/chalk_red_impure`
+        },
+        {
+            output: 'occultism:chalk_blue_impure',
+            inputs: ['occultism:chalk_black_impure', 'occultism:marid_essence', '#c:dusts/echo', '#c:dusts/echo'],
+            id: `occultism:crafting/chalk_blue_impure`
         }
     ];
 
