7b78f3772b376c0fd07e3d74497e646faac1772f
MuteTiefling <mutetiefling@gmail.com>
2025-02-15 10:53:07 -0500

    unify Oritech augment ingredients

---
+++ kubejs/server_scripts/recipes/oritech/augment.js
@@ -0,0 +1,31 @@
+ServerEvents.recipes((event) => {
+    const id_prefix = 'enigmatica:oritech/augment/';
+
+    const recipes = [];
+
+    event.forEachRecipe({ type: 'oritech:augment' }, (r) => {
+        let recipe = JSON.parse(r.json);
+        let recipe_id = r.getId();
+
+        let containers = ['applyCost', 'researchCost'];
+
+        unifyTags(recipe);
+        containers.forEach((container) => {
+            recipe[container].forEach((input) => {
+                if (input.ingredient.tag && AlmostUnified.getTagTargetItem(input.ingredient.tag) != null) {
+                    input.ingredient = { item: AlmostUnified.getTagTargetItem(input.ingredient.tag).getId() };
+                    recipe.id = recipe_id;
+                }
+            });
+        });
+
+        if (recipe.id) {
+            recipes.push(recipe);
+        }
+    });
+
+    recipes.forEach((recipe) => {
+        recipe.type = 'oritech:augment';
+        event.custom(recipe).id(recipe.id);
+    });
+});
