2f30ba77e8caa941385d8e9fad53c7030d93719b
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2025-03-31 19:22:35 +0200

    prevent soul gem from picking up `ars_nouveau:alakarkinos` #414

--- kubejs/server_scripts/tags/entity_type/occultism/soul_gem_deny_list.js
+++ kubejs/server_scripts/tags/entity_type/occultism/soul_gem_deny_list.js
@@ -2,5 +2,5 @@ ServerEvents.tags('entity_type', (event) => {
     event
         .get('occultism:soul_gem_deny_list')
         .remove('minecraft:wither')
-        .add([/ars_(nouveau|elemental):.*familiar/]);
+        .add([/ars_(nouveau|elemental):.*familiar/, 'ars_nouveau:alakarkinos']);
 });
