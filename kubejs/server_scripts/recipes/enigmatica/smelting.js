009bb41bcbdd6505f001683c0a880a09945231db
MuteTiefling <mutetiefling@gmail.com>
2025-07-02 19:10:56 -0400

    re-add missing smelting recipes
    likely got removed in recent unification efforts, re-adding.

---
+++ kubejs/server_scripts/recipes/enigmatica/smelting.js
@@ -0,0 +1,44 @@
+ServerEvents.recipes((event) => {
+    const id_prefix = 'enigmatica:smelting/';
+    const recipes = [
+        {
+            output: AlmostUnified.getTagTargetItem(`c:ingots/copper`).getId(),
+            input: '#c:dusts/copper',
+            xp: 0.7,
+            cookingTime: 200,
+            id: `${id_prefix}copper_ingot`
+        },
+        {
+            output: AlmostUnified.getTagTargetItem(`c:ingots/gold`).getId(),
+            input: '#c:dusts/gold',
+            xp: 0.7,
+            cookingTime: 200,
+            id: `${id_prefix}gold_ingot`
+        },
+        {
+            output: AlmostUnified.getTagTargetItem(`c:ingots/iron`).getId(),
+            input: '#c:dusts/iron',
+            xp: 0.7,
+            cookingTime: 200,
+            id: `${id_prefix}iron_ingot`
+        },
+        {
+            output: AlmostUnified.getTagTargetItem(`c:ingots/nickel`).getId(),
+            input: '#c:dusts/nickel',
+            xp: 0.7,
+            cookingTime: 200,
+            id: `${id_prefix}nickel_ingot`
+        },
+        {
+            output: AlmostUnified.getTagTargetItem(`c:ingots/zinc`).getId(),
+            input: '#c:dusts/zinc',
+            xp: 0.7,
+            cookingTime: 200,
+            id: `${id_prefix}zinc_ingot`
+        }
+    ];
+
+    recipes.forEach((recipe) => {
+        event.smelting(recipe.output, recipe.input, recipe.xp, recipe.cookingTime).id(recipe.id);
+    });
+});
