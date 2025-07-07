6c08032e39999f06b0b87faa1b0acd65ef39000b
MuteTiefling <mutetiefling@gmail.com>
2025-07-07 09:24:12 -0400

    Fix Steel smelting
    Steel is un smeltable. Fixed.

--- kubejs/server_scripts/recipes/enigmatica/blasting.js
+++ kubejs/server_scripts/recipes/enigmatica/blasting.js
@@ -1,42 +1,18 @@
 ServerEvents.recipes((event) => {
     const id_prefix = 'enigmatica:blasting/';
-    const recipes = [
-        {
-            output: AlmostUnified.getTagTargetItem(`c:ingots/copper`).getId(),
-            input: '#c:dusts/copper',
+    const recipes = [];
+
+    const materials = ['copper', 'gold', 'iron', 'nickel', 'zinc', 'steel'];
+
+    materials.forEach((material) => {
+        recipes.push({
+            output: AlmostUnified.getTagTargetItem(`c:ingots/${material}`).getId(),
+            input: `#c:dusts/${material}`,
             xp: 0.7,
             cookingTime: 100,
-            id: `${id_prefix}copper_ingot`
-        },
-        {
-            output: AlmostUnified.getTagTargetItem(`c:ingots/gold`).getId(),
-            input: '#c:dusts/gold',
-            xp: 0.7,
-            cookingTime: 100,
-            id: `${id_prefix}gold_ingot`
-        },
-        {
-            output: AlmostUnified.getTagTargetItem(`c:ingots/iron`).getId(),
-            input: '#c:dusts/iron',
-            xp: 0.7,
-            cookingTime: 100,
-            id: `${id_prefix}iron_ingot`
-        },
-        {
-            output: AlmostUnified.getTagTargetItem(`c:ingots/nickel`).getId(),
-            input: '#c:dusts/nickel',
-            xp: 0.7,
-            cookingTime: 100,
-            id: `${id_prefix}nickel_ingot`
-        },
-        {
-            output: AlmostUnified.getTagTargetItem(`c:ingots/zinc`).getId(),
-            input: '#c:dusts/zinc',
-            xp: 0.7,
-            cookingTime: 100,
-            id: `${id_prefix}zinc_ingot`
-        }
-    ];
+            id: `${id_prefix}${material}_ingot`
+        });
+    });
 
     recipes.forEach((recipe) => {
         event.blasting(recipe.output, recipe.input, recipe.xp, recipe.cookingTime).id(recipe.id);
