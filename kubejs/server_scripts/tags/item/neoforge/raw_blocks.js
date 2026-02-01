38ebcdbd82a64857a3bb62c30f264104981ada5f
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2026-02-01 20:06:13 +0100

    all of the raw tags

--- kubejs/server_scripts/tags/item/neoforge/raw_blocks.js
+++ kubejs/server_scripts/tags/item/neoforge/raw_blocks.js
@@ -10,9 +10,25 @@ ServerEvents.tags('item', (event) => {
         iridium: ['modern_industrialization:raw_iridium_block'],
         silver: ['modern_industrialization:raw_silver_block', 'immersiveengineering:raw_block_silver'],
         zinc: ['modern_industrialization:raw_zinc_block'],
+        tin: ['modern_industrialization:raw_tin_block'],
+        platinum: ['modern_industrialization:raw_platinum_block'],
+        antimony: ['modern_industrialization:raw_antimony_block'],
+        nickel: ['modern_industrialization:raw_nickel_block'],
+        titanium: ['modern_industrialization:raw_titanium_block'],
+        tungsten: ['modern_industrialization:raw_tungsten_block'],
+        uranium: ['modern_industrialization:raw_uranium_block'],
 
         // Immersive Engineering
-        aluminum: ['immersiveengineering:raw_block_aluminum']
+        aluminum: ['immersiveengineering:raw_block_aluminum'],
+
+        // Occultism
+        iesnium: ['occultism:raw_iesnium_block'],
+
+        // Create
+        zinc: ['create:raw_zinc_block'],
+
+        // Malum
+        raw_soulstone:['malum:block_of_raw_soulstone']
     };
 
     Object.keys(additions).forEach((tag) => {
